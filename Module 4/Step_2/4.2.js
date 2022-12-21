3d30d4b43fff800a03574c97d126fb9fb9085b33
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-12-21 22:40:44 +0200

    Code added

--- Module 4/Step_2/4.2.js
+++ Module 4/Step_2/4.2.js
@@ -1,14 +1,55 @@
 'use strict';
 
 const search = document.getElementById('search');
+
 search.addEventListener('click', function () {
+    let article= document.getElementById('movie_info');
+    article.innerHTML = '';
     const input = document.getElementById('input').value;
-    const url = 'https://api.tvmaze.com/singlesearch/shows?q=' + input;
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => console.log(error))
+    console.log(input);
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://api.tvmaze.com/search/shows?q=' + input);
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            const result = JSON.parse(request.responseText);
+            appendHTML(result[0]);
+        } else {
+            console.log('Error');
+        }
+
+    };
+    request.onerror = function () {
+        console.log('Error');
+
+    };
+    request.send();
 });
 
+function appendHTML(data) {
+
+    let article = document.getElementById('movie_info');
+
+    let paragraph1 = document.createElement('p');
+    paragraph1.innerText = 'Search result:'
+    article.append(paragraph1);
+
+    let h2 = document.createElement('h2');
+    h2.innerText = data.show.name;
+    article.append(h2);
+
+    let image = document.createElement('img');
+    image.src = data.show.image.medium;
+    image.alt = 'Show picture';
+    article.append(image);
+
+    let paragraph2 = document.createElement('p');
+    paragraph2.innerHTML = data.show.summary
+    article.append(paragraph2);
+
+    let link = document.createElement('a');
+    link.setAttribute('target', "_blank");
+    link.href = data.show.url;
+    link.innerText = 'Link to the original page';
+    article.append(link);
+
+}
